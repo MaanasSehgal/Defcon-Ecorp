294d97cc4f87462315cca21f26a337b7f72e552e
Akangkha Sarkar <akangkhasarkar@gmail.com>
2024-03-28 04:14:09 +0530

    ui changes in vault page

--- app/vault/page.jsx
+++ app/vault/page.jsx
@@ -1,22 +1,33 @@
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import "./vault.css";
 
 const page = () => {
-    return (
-        <>
-            <div className="text-[7rem] evilcorp-font text-custom-green mb-4 absolute text-center top-3 ">Evilcorp's Vault</div>
+  return (
+    <div className="border-2 border-custom-green flex flex-col gap-12 p-8 main">
+      <div className="text-[7rem] evilcorp-font text-custom-green mb-4 text-center ">
+        Evilcorp's Vault
+      </div>
 
-            <form method="POST" action="">
-                <div className="flex flex-row justify-center items-center">
-                    <input type="password" name="phrase" placeholder="Enter the phrase" className="bg-transparent outline-none border border-b-custom-green  border-transparent text-white caret-custom-green" />
-                <button type="submit" value="Submit" className=" text-custom-green text-2xl font-bold p-2 rounded-md mt-4 cursor-pointer">
-                    →
-                </button>
-                </div>
-            </form>
-        </>
-    );
+      <form method="POST" action="">
+        <div className="flex flex-row justify-center items-center border-2 border-dotted  border-custom-green">
+          <input
+            type="password"
+            name="phrase"
+            placeholder="enter the phrase"
+            className="bg-transparent outline-none w-fit text-white caret-custom-green"
+          />
+          <button
+            type="submit"
+            value="Submit"
+            className=" text-custom-green text-2xl font-bold  rounded-md cursor-pointer"
+          >
+            →
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default page;
