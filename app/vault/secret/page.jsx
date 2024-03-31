e254bd200eb81f9cad513142d3e33413ddba06ab
swappy <59965507+rycerzes@users.noreply.github.com>
2024-03-31 13:10:37 +0530

    changed page message

--- app/vault/secret/page.jsx
+++ app/vault/secret/page.jsx
@@ -3,8 +3,8 @@ import "./Secret.css";
 const page = () => {
     return (
         <div className="w-screen h-screen flex justify-center items-center">
-            <h1 className="text-custom-green text-9xl evilcorp-font">Secret page</h1>
-        </div>
+  <h1 className="text-custom-green text-8xl evilcorp-font glow-blink">Hacked the competition? Congrats!</h1>
+</div>  
     );
 };
 
